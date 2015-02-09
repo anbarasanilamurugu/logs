4791cc4e563e154aada48034ac0090d3beadc5b0
Mahendra Soni <mmahendra.soni@gmail.com>
2015-02-09 10:42:43 +0530

    better to use same naming convent at all locations

--- Log.php
+++ Log.php
@@ -7,24 +7,14 @@ if (login_check($mysqli) == false) {
 	header('Location:index.php');
 }
 
-/**
- * Require the library
- */
 require 'Log.class.php';
-/**
- * Initilize a new instance of PHPTail
- * @var PHPTail
- */
-$tail = new Log(APACHE_PATH);
 
-/**
- * We're getting an AJAX call
- */
+$log = new Log(APACHE_PATH);
+
+
 if(isset($_GET['ajax']))  {
-        echo $tail->getNewLines($_GET['pagenum']);
+        echo $log->getNewLines($_GET['pagenum']);
         die();
 }
-/**
- * Regular GET/POST call, print out the GUI
- */
-$tail->generateGUI();
+
+$log->generateGUI();
